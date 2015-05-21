f72105c70d155d5bdfa2812f67efbec112c2dc71
yulkes <eyal@meekan.com>
2015-05-21 21:51:20 +1200

    First commit

---
+++ MKNWorkDays/MKNWorkDays.h
@@ -0,0 +1,13 @@
+//
+//  MKNWorkDays.h
+//  MKNWorkDays
+//
+//  Created by Eyal Yavor on 21/5/15.
+//  Copyright (c) 2015 Meekan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MKNWorkDays : NSObject
+
+@end
---
+++ MKNWorkDays/MKNWorkDays.m
@@ -0,0 +1,13 @@
+//
+//  MKNWorkDays.m
+//  MKNWorkDays
+//
+//  Created by Eyal Yavor on 21/5/15.
+//  Copyright (c) 2015 Meekan. All rights reserved.
+//
+
+#import "MKNWorkDays.h"
+
+@implementation MKNWorkDays
+
+@end
